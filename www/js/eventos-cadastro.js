66f80613818709b907dd40ce557865d262fce010
Kamila Rocha <krp@discente.ifpe.edu.br>
2025-02-07 18:14:47 -0300

    alteração na tela de agenda colaborativa

--- www/js/eventos-cadastro.js
+++ www/js/eventos-cadastro.js
@@ -7,8 +7,11 @@ document.getElementById('eventoForm').addEventListener('submit', function(event)
   const promotor = document.getElementById('promotor').value;
   const local = document.getElementById('local').value;
   const estado = document.getElementById('estado').value;
-  const data = document.getElementById('data').value;
-  const horario = document.getElementById('horario').value;
+  const dataInicio = document.getElementById('data-inicio').value;
+  const horarioInicio = document.getElementById('horario-inicio').value;
+  const dataFim = document.getElementById('data-fim').value;
+  const horarioFim = document.getElementById('horario-fim').value;
+  
   let telefone = document.getElementById('telefone').value.replace(/\D/g, ''); // Remove caracteres não numéricos
   const email = document.getElementById('email').value;
   const link = document.getElementById('link').value;
@@ -28,18 +31,24 @@ document.getElementById('eventoForm').addEventListener('submit', function(event)
   }
 
   // Verificação dos campos obrigatórios
-  if (!titulo || !descricao || !promotor || !local || !estado || !data || !horario || (!email && !telefone)) {
+  if (!titulo || !descricao || !promotor || !local || !estado || !dataInicio || !horarioInicio || !dataFim || !horarioFim || (!email && !telefone)) {
     alert('Todos os campos obrigatórios devem ser preenchidos. Preencha o telefone ou o e-mail para contato.');
     return;
   }
 
   // Ajusta a data para evitar problemas de fuso horário
-  const dataEvento = new Date(data + 'T00:00:00');
+  const dataInicioEvento = new Date(`${dataInicio}T${horarioInicio}`);
+  const dataFimEvento = new Date(`${dataFim}T${horarioFim}`);
   const hoje = new Date();
   hoje.setHours(0, 0, 0, 0);
 
-  if (dataEvento < hoje) {
-    alert('A data do evento não pode ser anterior à data atual.');
+  if (dataInicioEvento < hoje) {
+    alert('A data de início do evento não pode ser anterior à data atual.');
+    return;
+  }
+
+  if (dataFimEvento < dataInicioEvento) {
+    alert('A data de fim do evento não pode ser anterior à data de início.');
     return;
   }
 
@@ -72,8 +81,10 @@ document.getElementById('eventoForm').addEventListener('submit', function(event)
       promotor,
       local,
       estado,
-      data: dataEvento.toISOString().split('T')[0],
-      horario,
+      dataInicio,
+      horarioInicio,
+      dataFim,
+      horarioFim,
       telefone,
       email,
       link,
--- www/js/eventos.js
+++ www/js/eventos.js
@@ -5,29 +5,18 @@ document.addEventListener('DOMContentLoaded', function () {
     const estadoFiltro = document.getElementById('filtroEstado').value;
     const dataAtual = new Date();
     dataAtual.setHours(0, 0, 0, 0);
-  
+
     const eventosFiltrados = eventos.filter(evento => {
-      const partesData = evento.data.split('-');
-      const dataEvento = new Date();
-      dataEvento.setFullYear(parseInt(partesData[0]), parseInt(partesData[1]) - 1, parseInt(partesData[2]));
-      dataEvento.setHours(0, 0, 0, 0);
-      return (!estadoFiltro || evento.estado === estadoFiltro) && dataEvento >= dataAtual;
+      const dataInicioEvento = new Date(`${evento.dataInicio}T${evento.horarioInicio}`);
+      return (!estadoFiltro || evento.estado === estadoFiltro) && dataInicioEvento >= dataAtual;
     });
-  
+
     eventosFiltrados.sort((a, b) => {
-      const dataA = new Date();
-      const partesA = a.data.split('-');
-      dataA.setFullYear(parseInt(partesA[0]), parseInt(partesA[1]) - 1, parseInt(partesA[2]));
-      dataA.setHours(0, 0, 0, 0);
-      
-      const dataB = new Date();
-      const partesB = b.data.split('-');
-      dataB.setFullYear(parseInt(partesB[0]), parseInt(partesB[1]) - 1, parseInt(partesB[2]));
-      dataB.setHours(0, 0, 0, 0);
-      
+      const dataA = new Date(`${a.dataInicio}T${a.horarioInicio}`);
+      const dataB = new Date(`${b.dataInicio}T${b.horarioInicio}`);
       return dataA - dataB;
     });
-  
+
     exibirEventos(eventosFiltrados);
   }
 
@@ -43,12 +32,12 @@ document.addEventListener('DOMContentLoaded', function () {
     eventos.forEach(evento => {
       const divEvento = document.createElement('div');
       divEvento.classList.add('evento');
-      
-      // Agora a imagem é um Base64 válido, então usamos diretamente no src
+
       const imagemEvento = evento.imagem ? evento.imagem : 'img/tela_eventos_imagem_perfil.png';
 
-      const partesData = evento.data.split('-');
-      const dataFormatada = `${partesData[2]}/${partesData[1]}/${partesData[0]}`;
+      // Formatação das datas
+      const dataInicioFormatada = evento.dataInicio.split('-').reverse().join('/');
+      const dataFimFormatada = evento.dataFim.split('-').reverse().join('/');
 
       divEvento.innerHTML = `
         <img src="${imagemEvento}" alt="Imagem do evento" id="imgEvento" class="card-image">
@@ -57,8 +46,8 @@ document.addEventListener('DOMContentLoaded', function () {
         <p><strong>Promovido por:</strong> ${evento.promotor}</p>
         <p><strong>Local:</strong> ${evento.local}</p>
         <p><strong>Estado:</strong> ${evento.estado}</p>
-        <p><strong>Data:</strong> ${dataFormatada}</p>
-        <p><strong>Horário:</strong> ${evento.horario}</p>
+        <p><strong>Data de Início:</strong> ${dataInicioFormatada} às ${evento.horarioInicio}</p>
+        <p><strong>Data de Fim:</strong> ${dataFimFormatada} às ${evento.horarioFim}</p>
         <p><strong>Telefone:</strong> ${evento.telefone}</p>
         <p><strong>E-mail:</strong> <a href="mailto:${evento.email}">${evento.email}</a></p>
         <p><strong>Link:</strong> <a href="${evento.link}" target="_blank">${evento.link}</a></p>
